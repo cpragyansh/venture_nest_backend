7788db27aa51a9e213a216fdacc60e0c4d7f6143
Pragyansh Chauhan <cpragyansh@gmail.com>
2025-03-22 00:41:03 +0530

    admin page origin

--- config/AllowedOrigan.js
+++ config/AllowedOrigan.js
@@ -4,7 +4,8 @@ const allowedOrigins = [
     'http://localhost:3001', // Local development
     'http://localhost:5000', // Local development
     'http://localhost:3002', // Local development
-    'https://venturenest-a9a81.firebaseapp.com', // Production domain
+    'https://venturenest-a9a81.firebaseapp.com', // Production domain,
+    "https://ventureadmin-c530a.web.app"
 ];
 
 module.exports = allowedOrigins;
