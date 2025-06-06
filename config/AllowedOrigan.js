727769dde79a830a10888d706d14d2f50beb3c85
Pragyansh Chauhan <cpragyansh@gmail.com>
2025-06-06 06:56:54 +0530

    origin updated

--- config/AllowedOrigan.js
+++ config/AllowedOrigan.js
@@ -6,7 +6,9 @@ const allowedOrigins = [
     'http://localhost:3002', // Local development
     'http://localhost:5173', // Local development
     'https://venturenest-a9a81.firebaseapp.com', // Production domain,
-    "https://ventureadmin-c530a.web.app"
+    "https://ventureadmin-c530a.web.app",
+    "https://venture-e1ef7.web.app".
+    
 ];
 
 module.exports = allowedOrigins;
